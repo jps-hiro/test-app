ed00ecfa9dc4d758a4495d7eb99b68ff601bdeaf
JPerfectSolution <j.perfectsolution@outlook.com>
2020-06-23 01:39:05 -0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test-app';
+  items = [
+    'Blood Glucose',
+    'Medication',
+    'Food Tracking',
+    'Exercise',
+    'Weight Management',
+    'Blood Pressure',
+    'A1C',
+  ];
+  selectedItem = 'Blood Glucose';
+  selectedNav = 0;
+  navs = [0, 1, 2, 3, 4];
+
+  back() {
+    if (this.selectedNav > 0) this.selectedNav--;
+  }
+  next() {
+    if (this.selectedNav < this.navs[this.navs.length - 1]) this.selectedNav++;
+  }
 }
